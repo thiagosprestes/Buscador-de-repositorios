e70be6acd46dee07a4913df239f4a927b778a73d
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-02-26 20:10:00 -0300

    Filtro de ordenação transferido para bloco com os rsultados de busca

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import SearchResults from './components/searchResults';
 
 function App() {
   const [ results, setResults ] = useState([])
+  const [ totalItems, setTotalItems] = useState('')
+  const [ sort, setSort ] = useState('')
   const [ notFound, setNotFound] = useState('')
 
   async function handleSubmitSearch(data) {
-    api.get(`${data.searchTerm}&per_page=4&order=desc&sort=${data.sort}`)
+    api.get(`${data}&per_page=4&order=desc&sort=${sort}`)
     .then(response => {
       setResults(response.data.items)
+      setTotalItems(response.data.total_count)
       
       if(response.data.items == 0) {
         setNotFound('Nenhum repositório foi encontrado com este termo de busca')
@@ -30,8 +33,18 @@ function App() {
         <h4 style={{marginTop: '20px'}}>{notFound}</h4>
       )}
 
-      {results != 0 && (
+      {results  && (
         <div className="input-block">
+          <div className="header">
+            <strong>{totalItems} repositórios encontrados</strong>
+            <div className="sort-options">
+              <label>Filtrar:</label>
+              <select name="sort" id="sort" value={sort} onChange={e => setSort(e.target.value)}>
+                <option value="undefined">Melhores resultados</option>
+                <option value="stars">Mais estrelas</option>
+              </select>
+            </div>
+          </div>
           <ul>
             {results.map(data => (
               <SearchResults key={data.id} item={data} />
--- src/components/searchForm/index.js
+++ src/components/searchForm/index.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 
 function SearchForm({ onSubmit }) {
     const [ searchTerm, setSearchTerm ] = useState('')
-    const [ sort, setSort ] = useState('')
 
     async function handleSubmit() {
-        await onSubmit({searchTerm, sort})
+        await onSubmit(searchTerm)
     }
 
     return (
@@ -13,11 +12,6 @@ function SearchForm({ onSubmit }) {
             <label htmlFor="searchTerm">Seu termo de busca</label>
             <div className="input-group">
                 <input type="text" name="searchTerm" id="searchTerm" onKeyUp={handleSubmit} onChange={e => setSearchTerm(e.target.value)} />
-                <select name="sort" id="sort" onChange={e => setSort(e.target.value)}>
-                    <option disabled selected>Ordenação</option>
-                    <option value="">Melhor resultado</option>
-                    <option value="stars">Mais estrelas</option>
-                </select>
             </div>
         </div>
     )
