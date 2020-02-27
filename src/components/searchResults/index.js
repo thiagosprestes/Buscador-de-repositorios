392167e98f6e3bdd96a756cd62a5262f3d49a54a
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-02-27 13:47:03 -0300

    Adicionado botão com link do repositório no github.

--- src/components/searchResults/index.js
+++ src/components/searchResults/index.js
@@ -9,11 +9,16 @@ function SearchResults({ item }) {
             <strong>{item.full_name}</strong>
             <p className="description">{item.description}</p>
             <div className="repository-info">
-                <div className="stars">                    
-                    {item.stargazers_count} stars
+                <div className="data">
+                    <div className="stars">                    
+                        {item.stargazers_count} stars
+                    </div>
+                    {item.language !== null && <p className="language">{item.language}</p>}
+                    <p className="date">Updated {moment(new Date(item.pushed_at)).format('D MMMM YYYY')}</p>
                 </div>
-                {item.language !== null && <p className="language">{item.language}</p>}
-                <p className="date">Updated {moment(new Date(item.pushed_at)).format('D MMMM YYYY')}</p>
+                <div className="action">
+                    <a href={item.html_url} target="_blank">Ver no Github</a>
+                </div>
             </div>
         </li>
     )
